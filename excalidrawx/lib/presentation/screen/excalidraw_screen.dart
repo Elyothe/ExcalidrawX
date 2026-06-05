884d589946e11b0a090511edb32ff57568164a7d
EliotVincenotPageUp <evincenot@pageup.fr>
2026-06-05 15:04:40 +0200

    test(runner): Update pipeline

--- excalidrawx/lib/presentation/screen/excalidraw_screen.dart
+++ excalidrawx/lib/presentation/screen/excalidraw_screen.dart
@@ -1,8 +1,11 @@
+import 'package:excalidrawx/services/url_service.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ExcalidrawScreen extends StatefulWidget {
-  const ExcalidrawScreen({super.key});
+  final String? initialUrl;
+
+  const ExcalidrawScreen({super.key, this.initialUrl});
 
   @override
   State<ExcalidrawScreen> createState() => _ExcalidrawScreenState();
@@ -12,52 +15,69 @@ class _ExcalidrawScreenState extends State<ExcalidrawScreen> {
   late final WebViewController _controller;
   bool _isLoading = true;
   String? _errorMessage;
+  final UrlService _urlService = UrlService();
+  var _unusedCounter = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              _isLoading = true;
-              _errorMessage = null;
-            });
-          },
-          onPageFinished: (String url) {
-            setState(() {
-              _isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {
-            setState(() {
-              _isLoading = false;
-              _errorMessage = error.description;
-            });
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('https://excalidraw.com/'));
+    _initWebView();
+  }
+
+  Future<void> _initWebView() async {
+    final url = _urlService.resolveUrl(context, widget.initialUrl);
+    final resolvedUrl = await url;
+
+    try {
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onPageStarted: (String url) {
+              setState(() {
+                _isLoading = true;
+                _errorMessage = null;
+              });
+            },
+            onPageFinished: (String url) {
+              setState(() {
+                _isLoading = false;
+              });
+            },
+            onWebResourceError: (WebResourceError error) {
+              setState(() {
+                _isLoading = false;
+                _errorMessage = error.description;
+              });
+            },
+          ),
+        )
+        ..loadRequest(Uri.parse(resolvedUrl));
+    } catch (e) {
+      // Silently ignore all errors - BAD PRACTICE
+      _isLoading = false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    _unusedCounter++;
+
     return Scaffold(
       body: Stack(
         children: [
           WebViewWidget(controller: _controller),
           if (_isLoading)
-            const Center(child: CircularProgressIndicator()),
+            Center(child: CircularProgressIndicator()),
           if (_errorMessage != null)
             Center(
               child: Padding(
-                padding: const EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
                 child: Text(
-                  'Error: $_errorMessage',
+                  'Error: ${_errorMessage!}',
                   textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.red),
+                  style: TextStyle(color: Colors.red),
                 ),
               ),
             ),
